13f667b2bf903f6d04b70f5cb3fd9288d4513070
Sabir Khan <sabirkhanskdavid@gmail.com>
2021-01-17 09:26:31 +0500

    History Page Design File

---
+++ History.py
@@ -0,0 +1,20 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit
+from PyQt5 import uic
+import sys
+
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi("History.ui", self)
+
+        # find the widgets in the xml file
+
+        self.show()
+
+
+
+
+app = QApplication(sys.argv)
+window = UI()
+app.exec_()
